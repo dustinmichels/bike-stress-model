c9b7f029263bd9898ddfdad749e581b8bce94af5
Dustin Michels <dustin7538@gmail.com>
2025-11-26 14:09:52 -0500

    extract width

--- osm.py
+++ osm.py
@@ -4,8 +4,10 @@ import numpy as np
 import osmnx as ox
 import pandas as pd
 
+FEET_TO_M = 0.3048
 
-def extract_maxspeed(x):
+
+def extract_maxspeed(x) -> float:
     # Handle NaN early
     if x is None or (isinstance(x, float) and np.isnan(x)):
         return np.nan
@@ -24,10 +26,38 @@ def extract_maxspeed(x):
     return int(max(nums, key=int)) if nums else np.nan
 
 
+def extract_width(value):
+    # If list, use first value
+    if isinstance(value, list):
+        value = value[0]
+
+    """Return numeric width in meters from OSM width tags."""
+    if pd.isna(value):
+        return None
+
+    s = str(value).strip()
+
+    # Feet notation like 9'
+    if "'" in s:
+        match = re.search(r"\d+(\.\d+)?", s)
+        if match:
+            return float(match.group()) * FEET_TO_M
+        else:
+            return None
+
+    # Standard numeric meters
+    match = re.search(r"\d+(\.\d+)?", s)
+    if match:
+        return float(match.group())
+
+    return None
+
+
 def get_network(place: str, network_type: str):
     G = ox.graph_from_place(place, network_type=network_type)
     _, edges = ox.graph_to_gdfs(G)
     edges["maxspeed_int"] = edges["maxspeed"].apply(extract_maxspeed)
+    edges["width_float"] = edges["width"].apply(extract_width)
     return edges
 
 
