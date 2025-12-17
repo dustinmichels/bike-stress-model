8dc6cd2275a7ae5dc9002404b9e24b5eaf59c41c
Dustin Michels <dustin7538@gmail.com>
2025-12-17 15:27:11 -0500

    update backend notebook and copy script

--- copy.sh
+++ copy.sh
@@ -1,10 +1,15 @@
 # !/bin/zsh
 
 # copy somerville
-cp backend/data/out/somerville_streets.geojson frontend/public/somerville_streets.geojson
+cp backend/data/out/main/somerville_streets.geojson frontend/public/somerville_streets.geojson
 
 # copy cambridge
-cp backend/data/out/cambridge_streets.geojson frontend/public/cambridge_streets.geojson
+cp backend/data/out/main/cambridge_streets.geojson frontend/public/cambridge_streets.geojson
 
 # copy everett
-cp backend/data/out/everett_streets.geojson frontend/public/everett_streets.geojson
+cp backend/data/out/main/everett_streets.geojson frontend/public/everett_streets.geojson
+
+# copy charts, if any
+cp backend/data/out/notebook/chart*.html deployed-charts
+
+
